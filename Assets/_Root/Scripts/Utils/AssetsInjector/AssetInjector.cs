0ff5e9c176647bb99dc40df4119611e1311fbb17
Svetlana Golubeva <lanagolubshu@gmail.com>
2022-07-05 14:37:20 +0300

    Update (Added injector realisation)

--- Assets/_Root/Scripts/Utils/AssetsInjector/AssetInjector.cs
+++ Assets/_Root/Scripts/Utils/AssetsInjector/AssetInjector.cs
@@ -1,7 +1,31 @@
-﻿public static class AssetInjector
+﻿using System;
+using System.Reflection;
+using UnityEngine;
+
+public static class AssetInjector
 {
+	private static readonly Type injectAssetAttribute = typeof(InjectAssetAttribute);
+
 	public static T Inject<T>(this AssetContext context, T target)
 	{
+		Type targetType = target.GetType();
+		FieldInfo[] targetFields = targetType.GetFields(BindingFlags.NonPublic |
+												BindingFlags.Instance |
+												BindingFlags.Public);
+
+        for (int i = 0; i < targetFields.Length; i++)
+        {
+			FieldInfo fieldInfo = targetFields[i];
+			InjectAssetAttribute _injectAssetAttrubute = fieldInfo.GetCustomAttribute(injectAssetAttribute) as InjectAssetAttribute;
+			if (_injectAssetAttrubute == null)
+				continue;
+
+			var objectToInject = context.GetObjectOfType(fieldInfo.FieldType, _injectAssetAttrubute.assetName);
+			fieldInfo.SetValue(target, objectToInject);
+		}
+
+
+
 		return target;
 	}
 }
